e4f0217c2114ed004b9449300259c353ead65b66
Giles Bowkett <gilesb@gmail.com>
2008-10-12 02:14:24 -0700

    This is a stupid script to open BoingBoing if you press the center button on a WiiMote.
    It will also open BoingBoing if you hit a middle-ish A (note 60) on a synth.

---
+++ wiimote.rb
@@ -0,0 +1,16 @@
+require 'coremidi'
+require 'midi_in'
+require 'rubygems'
+require 'active_support'
+
+# Now run this script
+
+midi_in = MidiIn.new
+midi_in.scan
+midi_in.link(0)
+midi_in.capture do |data|
+  data[0].data.in_groups_of(3) do |unknown, midi_note_number, unknown|
+    system("open http://boingboing.net") if 60 == midi_note_number
+  end
+end
+
